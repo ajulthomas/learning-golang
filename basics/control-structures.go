101ac4aba18d81d5b8b342b6d50b203ce38f6a2c
aulthomas <ajul.thomas@outlook.com>
2021-06-05 19:46:38 +0530

    adds more features of switch

--- basics/control-structures.go
+++ basics/control-structures.go
@@ -2,22 +2,42 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 
 	fmt.Println("Prints numbers from 1 to 10 using for loop")
-	printRange(1,10,"")
+	printRange(1, 10, "")
 	fmt.Println("Prints odd numbers from 1 to 10")
-	printRange(1,20,"odd")
+	printRange(1, 20, "odd")
 	fmt.Println("Prints even numbers from 1 to 30")
-	printRange(1,30,"even")
+	printRange(1, 30, "even")
+
+	// switch without condition is a clean way to execute long if else chains
+	// evaluates from top to bottom
+	switch {
+	case 1 < 2:
+		fmt.Println("can evaluate expression in a case")
+	case false:
+		fmt.Println("Prints first true statement")
+	case true:
+		fmt.Println("Prints first true statement")
+	default:
+		fmt.Println("Default condition gets executed if non of the cases evaluates to true")
+	}
 }
 
 func printRange(l int32, u int32, filterType string) {
-	for i:=l; i<=u; i++ {
+	for i := l; i <= u; i++ {
 		switch filterType {
-		case "odd": if i % 2 != 0 { fmt.Println(i) }
-		case "even": if i % 2 == 0 { fmt.Println(i) }
-		default: fmt.Println(i)	
+		case "odd":
+			if i%2 != 0 {
+				fmt.Println(i)
+			}
+		case "even":
+			if i%2 == 0 {
+				fmt.Println(i)
+			}
+		default:
+			fmt.Println(i)
 		}
 	}
 }
