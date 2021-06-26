2bc7a176c60cec53a279f0d7a8a151ffca118799
aulthomas <ajul.thomas@outlook.com>
2021-06-26 10:12:15 +0530

    creates a custom type JSON - a map of strings to strings

--- beyond-basics/maps.go
+++ beyond-basics/maps.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+type JSON map[string]string
+
 func main() {
 	var m = make(map[string]string)
 	m["name"] = "My Name"
@@ -18,4 +20,10 @@ func main() {
 	for key, value := range address {
 		fmt.Printf("key: %v, value: %v\n\n", key, value)
 	}
+	apiResponse := JSON{
+		"apiName":    "test",
+		"status":     "SUCCESS",
+		"statusCode": "200",
+	}
+	fmt.Printf("API response ===> %v", apiResponse)
 }
